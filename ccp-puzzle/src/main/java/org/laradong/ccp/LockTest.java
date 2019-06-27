5ad4f0d603b45f8e3e13c8c8b19483ceabb14cdf
laradong <lara.dong@163.com>
2019-06-27 21:23:20 +0800

    add ccp puzzle

---
+++ ccp-puzzle/src/main/java/org/laradong/ccp/LockTest.java
@@ -0,0 +1,18 @@
+package org.laradong.ccp;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class LockTest {
+    private final static Lock lock = new ReentrantLock();
+
+    public static void main(String[] args) {
+        try {
+            lock.tryLock();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+}
